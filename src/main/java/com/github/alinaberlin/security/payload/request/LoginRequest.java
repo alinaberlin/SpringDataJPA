ae282ab0db8bad65e6fbdaf5d64d3610d59fe21b
alinaberlin <alina.ghetler@gmail.com>
2024-01-01 21:21:00 +0100

    Implement JWT-based Authentication

--- src/main/java/com/github/alinaberlin/security/payload/request/LoginRequest.java
+++ src/main/java/com/github/alinaberlin/security/payload/request/LoginRequest.java
@@ -1,5 +1,50 @@
 package com.github.alinaberlin.security.payload.request;
 
-public class LoginRequest {
+import java.util.Objects;
 
+public class LoginRequest {
+    private String email;
+    private String pasword;
+
+    public LoginRequest(String email, String paswword) {
+        this.email = email;
+        this.pasword = paswword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasword() {
+        return pasword;
+    }
+
+    public void setPasword(String paswword) {
+        this.pasword = paswword;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LoginRequest that = (LoginRequest) o;
+        return Objects.equals(getEmail(), that.getEmail()) && Objects.equals(getPasword(), that.getPasword());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getEmail(), getPasword());
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "email='" + email + '\'' +
+                ", pasword='" + pasword + '\'' +
+                '}';
+    }
 }
