a7558f2b1ff9e21fc0cfcb692c1c4861de3199b8
alinaberlin <alina.ghetler@gmail.com>
2024-01-03 21:27:19 +0100

    Change the property service's unit tests in order to use Mokito

--- src/test/java/com/github/alinaberlin/restate/services/RealtorServiceTest.java
+++ src/test/java/com/github/alinaberlin/restate/services/RealtorServiceTest.java
@@ -5,38 +5,49 @@ import com.github.alinaberlin.restate.repositories.RealtorRepository;
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class RealtorServiceTest {
 
-    @Autowired
+
+    @Mock
+    private RealtorRepository mockRealtorRepository;
+
     private RealtorService realtorService;
 
-    @Autowired
-    private RealtorRepository realtorRepository;
 
     @AfterEach
     public void teardown(){
-        realtorRepository.deleteAll();
     }
-
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+        realtorService = new RealtorService( mockRealtorRepository);
+    }
     @Test
     void getAllRealtors() {
         List<Realtor> realtors  = Arrays.asList( new Realtor(1, "Alina", "alinagmail.com", "123455"),
                 new Realtor(2, "Alina2", "alinagmail.com", "123455"));
-       realtorRepository.saveAll(realtors);
+       Mockito.when(mockRealtorRepository.findAll()).thenReturn(realtors);
         List<Realtor> result = realtorService.getAllRealtors();
         Assertions.assertEquals(realtors, result);
 
@@ -45,7 +56,7 @@ class RealtorServiceTest {
     @Test
     void getRealtorById() {
         Realtor realtor = new Realtor(1, "Alina", "alinagmail.com", "123455");
-        realtorService.saveRealtor(realtor);
+        Mockito.when(mockRealtorRepository.findById(1L)).thenReturn(Optional.of(realtor));
         Realtor found  =realtorService.getRealtorById(1);
         Assertions.assertEquals( realtor, found);
     }
@@ -53,6 +64,7 @@ class RealtorServiceTest {
     @Test
     void saveRealtor() {
         Realtor realtor = new Realtor(1, "Alina", "alinagmail.com", "123455");
+        Mockito.when(mockRealtorRepository.save(realtor)).thenReturn(realtor);
         Realtor result = realtorService.saveRealtor(realtor);
         Assertions.assertEquals(new Realtor(1, "Alina", "alinagmail.com", "123455"), result);
     }
@@ -70,8 +82,10 @@ class RealtorServiceTest {
     @Test
     void updateRealtor() {
         Realtor realtor = new Realtor(1, "Alina", "alinagmail.com", "123455");
+        Mockito.when(mockRealtorRepository.save(realtor)).thenReturn(realtor);
         Realtor result = realtorService.saveRealtor(realtor);
         Realtor realtor1 = new Realtor(1, "Ghetler", "alinagmail.com", "123455");
+        Mockito.when(mockRealtorRepository.save(realtor1)).thenReturn(realtor1);
         Realtor result2= realtorService.updateRealtor(realtor1);
         Assertions.assertEquals(realtor1, result2);
 
